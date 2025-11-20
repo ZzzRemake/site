c1d2ec13b0aeb04d8b4571fe067cb7c40d755db6
ZzzRemake <remake114514@outlook.com>
2025-11-20 22:55:22 +0800

    [fix]: travel url modify.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -106,7 +106,7 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://www.travellings.cn/typewriter.html',
+            href: 'https://www.travellings.cn/go.html',
             label: 'Travelling',
             position: 'right',
           },
